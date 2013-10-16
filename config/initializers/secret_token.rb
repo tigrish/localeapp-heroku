16ce6b78a84a2a029c04071afd75ecbc925b0eb6
Christopher Dell <chris@tigrish.com>
2013-10-16 09:46:39 +0200

    rails new localeapp-heroku -O -S -J

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocaleappHeroku::Application.config.secret_key_base = 'a3be6c94d700481cf8e94eb3bad900eaf65066287022f589881474cef4c80142f059404d7678a3fa072a110860b20db20327417f4edab14fa48ee3c811322a5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LocaleappHeroku::Application.config.session_store :cookie_store, key: '_localeapp-heroku_session'
