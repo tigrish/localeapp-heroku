8f37641a7b0b9af46f8b3634fb6bedc2a95795ed
Christopher Dell <chris@tigrish.com>
2013-10-16 10:18:28 +0200

    Configure `localeapp` to poll and reload in production

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,6 +2,8 @@ require 'localeapp/rails'
 
 Localeapp.configure do |config|
   config.api_key = ENV['LOCALEAPP_API_KEY']
+  config.polling_environments   << :production
+  config.reloading_environments << :production
 
   # Pull latest when dyno restarts in production
   if defined?(Rails) && Rails.env.production?
