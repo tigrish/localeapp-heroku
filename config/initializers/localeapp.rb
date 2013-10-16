e822cd0d6bb8ae33d226376349bbed129f9ee579
Christopher Dell <chris@tigrish.com>
2013-10-16 10:15:36 +0200

    Pull latest translations after restarting dynos (also after deploying)

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,4 +2,9 @@ require 'localeapp/rails'
 
 Localeapp.configure do |config|
   config.api_key = ENV['LOCALEAPP_API_KEY']
+
+  # Pull latest when dyno restarts in production
+  if defined?(Rails) && Rails.env.production?
+    Localeapp::CLI::Pull.new.execute
+  end
 end
